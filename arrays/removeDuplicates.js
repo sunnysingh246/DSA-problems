1d59050fe44ab5af53fa4cb0211e217ba26194c2
Sunny Singh <sunnyk60825@gmail.com>
2026-07-02 18:10:14 +0530

    remove duplicate problem

---
+++ arrays/removeDuplicates.js
@@ -0,0 +1,11 @@
+var removeDuplicates=function(nums){
+    let j=1
+    for (let i = 0; i< nums.length-1; i++) {
+        if(nums[i]!=nums[i+1]){
+            nums[j]=nums[i+1]
+            j++
+        }
+        
+    }
+    return j
+}
