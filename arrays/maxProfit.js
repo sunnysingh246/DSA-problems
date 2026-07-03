9d4c0de6172f403c639f86f56d0362c2b3ef6f93
Sunny Singh <sunnyk60825@gmail.com>
2026-07-03 16:44:42 +0530

    maximum profit problem

---
+++ arrays/maxProfit.js
@@ -0,0 +1,13 @@
+function maxProfit(prices) {
+    let maxProfit = 0;
+    let min = prices[0]
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < min) min=prices[i]
+        let profit=prices[i]-min
+        maxProfit=Math.max(maxProfit,profit)
+    }
+    return maxProfit;
+}
+
+console.log(maxProfit([2,5,7,2,8,6,3,8]))
