be3d2c3287307520ac52821abfc7a765a239a5c4
Sunny Singh <sunnyk60825@gmail.com>
2026-07-04 17:26:59 +0530

    Finding missing numbers

--- arrays/multipleMissingNums.js
+++ arrays/multipleMissingNums.js
@@ -1,12 +1,33 @@
-function findMissingNumbers(arr) {
-    let missing = []
+// function findMissingNumbers(arr) {
+//     let missing = []
 
-    for (let i = 0; i <= arr[arr.length - 1]; i++) {
-        if (!arr.includes(i)) {
-            missing.push(i)
+//     for (let i = 0; i <= arr[arr.length - 1]; i++) {
+//         if (!arr.includes(i)) {
+//             missing.push(i)
+//         }
+//     }
+//     return missing
+// }
+
+// console.log(findMissingNumbers([1, 2, 5, 6, 8]));
+
+
+//2nd method
+
+function findMissingNumbers(arr) {
+    let n = Math.max(...arr);
+    let set = new Set(arr);
+    let missing = [];
+
+    for (let i = 1; i <= n; i++) {
+        if (!set.has(i)) {
+            missing.push(i);
         }
     }
-    return missing
+
+    return missing;
 }
 
-console.log(findMissingNumbers([1, 2, 5, 6, 8]));
+const arr = [1, 2, 5, 6, 8];
+
+console.log(findMissingNumbers(arr));
