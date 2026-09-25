3bf16f2074639b2a0347eed859ec03a010ee10ff
Sunny singh <sunnyk60825@gmail.com>
2026-09-25 10:29:31 +0530

    Linear search in array

--- arrays/linearSearch.js
+++ arrays/linearSearch.js
@@ -1,11 +1,27 @@
-function linearSearch(arr, target) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == target) return 1
+const linearSearch = function (arr, targert) {
+    for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] == targert) {
+            return i
+        }
     }
     return -1
 }
 
-const arr = [10, 20, 30, 40, 50];
+//console.log(linearSearch([1, 3, 5, 7, 3, 9, 0], 5))
 
-console.log(linearSearch(arr, 40));
-console.log(linearSearch(arr, 70));
+
+
+
+//Global Linear search
+const globalLinearSearch = function (arr, targert) {
+    const result = []
+    for (let i = 0; i < arr.length; i++) {
+        if (targert === arr[i]) {
+            result.push(i)
+        }
+    }
+    if (result.length === 0) result - 1
+    return result
+}
+
+console.log(globalLinearSearch([1, 3, 0, 5, 7, 3, 9, 0], 0))
